5f6c82d1e27950f40668d5ac2196544785c54953
Carlos Martín Nieto <cmn@dwim.me>
2015-03-25 08:42:36 +0100

    Don't lock on the session when incrementing the refcount
    The current comment says that it performs a full lock so the session
    doesn't get freed between reading the pointer and incrementing the
    refcount. Such a situation would however indicate bad reference
    counting, or some code freeing a session to which it doesn't hold a
    reference. This would be a bug in the caller and should be fixed there.

--- src/lib/libssl/src/ssl/ssl_sess.c
+++ src/lib/libssl/src/ssl/ssl_sess.c
@@ -160,16 +160,9 @@ SSL_get1_session(SSL *ssl)
 {
 	SSL_SESSION *sess;
 
-	/*
-	 * Need to lock this all up rather than just use CRYPTO_add so that
-	 * somebody doesn't free ssl->session between when we check it's
-	 * non-null and when we up the reference count.
-	 */
-	CRYPTO_w_lock(CRYPTO_LOCK_SSL_SESSION);
 	sess = ssl->session;
 	if (sess)
-		sess->references++;
-	CRYPTO_w_unlock(CRYPTO_LOCK_SSL_SESSION);
+		CRYPTO_refcount_inc(&sess->references);
 
 	return (sess);
 }
