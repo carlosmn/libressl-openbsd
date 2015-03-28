d5a85a726a7b6601b2dfa6739d2b4303b78f3726
Carlos Martín Nieto <cmn@dwim.me>
2015-03-28 13:34:11 +0100

    more

--- src/lib/libssl/src/crypto/cryptlib.c
+++ src/lib/libssl/src/crypto/cryptlib.c
@@ -568,16 +568,8 @@ CRYPTO_add_lock(int *pointer, int amount, int type, const char *file,
 	CRYPTO_lock(CRYPTO_LOCK|CRYPTO_WRITE, type, file, line);
 
 	ret= *pointer + amount;
-#ifdef LOCK_DEBUG
-	{
-		CRYPTO_THREADID id;
-		CRYPTO_THREADID_current(&id);
-		fprintf(stderr, "ladd:%08lx:%2d+%2d->%2d %-18s %s:%d\n",
-			CRYPTO_THREADID_hash(&id), *pointer, amount, ret,
-			CRYPTO_get_lock_name(type), file, line);
-	}
-#endif
 	*pointer = ret;
+
 	CRYPTO_lock(CRYPTO_UNLOCK|CRYPTO_WRITE, type, file, line);
 
 	return (ret);
